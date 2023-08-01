b0d95958ffd94e0d7ff6179a8b6129a9375d1842
Oleksii Pohasii <pogasiya@gmail.com>
2023-08-01 21:35:16 +0200

    up

--- homework/Gameclicker/script.js
+++ homework/Gameclicker/script.js
@@ -1,6 +1,6 @@
 function startGame() {
   object.classList.add("start");
-  // console.log("startGame() works");
+  console.log("startGame() works");
 }
 
 function miss(event) {
@@ -18,6 +18,15 @@ function hit() {
   object.classList.remove("start");
   void object.offsetWidth; //магия
   object.classList.add("start");
+  object.style.background = getRandomColor();
+}
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 let score = 0;
